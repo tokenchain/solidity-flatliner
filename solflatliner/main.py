194988d522a5b08a525948ce22e8fdf7e1361423
heskdx <jobhesk@gmail.com>
2022-06-13 16:38:32 +0800

    auto patched

--- solflatliner/main.py
+++ solflatliner/main.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import re
 import sys
+from os.path import exists
 
 DESCRIPTION = """
 Source Verification Unfolds.\n
@@ -17,8 +18,8 @@ def parse_arguments():
     parser.add_argument("version", type=str, metavar="*.*.*", help="solidity compiler version e.g. 0.4.24")
     parser.add_argument("-o", "--output", type=str, metavar="*.sol", help="output filename (default: flat.sol)")
     parser.add_argument("-d", "--ofolder", type=str, metavar="*", help="output folder (default: verify)", default="verify")
+    parser.add_argument("-lib", "--library", type=str, metavar="*", help="reference library folder (default: lib)", default="lib")
     args = parser.parse_args()
-
     return args
 
 
@@ -28,37 +29,45 @@ def is_sol_valid(infile, s):
         sys.exit(-2)
 
 
-def unfold_imports(imports, infile):
+def unfold_imports(library_folder: str, imports: list, infile: str, importline: str = ""):
     buffer = []
 
     if infile not in imports:
-        with open(infile, "r+") as f:
-            for line in f:
-                # Remove the pragma line in all imports
-                if "pragma" in line[:6]:
-                    continue
-                # Remove the LICENSE line in all imports
-                if "// SPDX-License-Identifier:" in line[:27]:
-                    continue
 
-                if "//SPDX-License-Identifier:" in line[:26]:
-                    continue
+        found = exists(infile)
 
-                # Read the imports
-                if "import" in line[:6]:
-                    match = re.search(r".*[\'|\"](.*)[\'|\"]", line)
-                    if match:
-                        dirname = os.path.dirname(infile)
-                        file = os.path.join(dirname, match.group(1))
-                        absfile = os.path.abspath(file)
+        if not found:
+            infile = os.path.join(library_folder, importline)
+        try:
+            with open(infile, "r+") as f:
+                for line in f:
+                    # Remove the pragma line in all imports
+                    if "pragma" in line[:6]:
+                        continue
+                    # Remove the LICENSE line in all imports
+                    if "//" in line[:2] and "SPDX-License-Identifier:" in line:
+                        continue
 
-                        buffer.append(unfold_imports(imports, absfile))
-                        imports.append(absfile)
+                    # Read the imports
+                    if "import" in line[:6]:
+                        match = re.search(r".*[\'|\"](.*)[\'|\"]", line)
+                        if match:
+                            dirname = os.path.dirname(infile)
+                            importline = match.group(1)
+                            file = os.path.join(dirname, importline)
+                            absfile = os.path.abspath(file)
+                            buffer.append(unfold_imports(library_folder, imports, absfile, importline))
+                            imports.append(absfile)
+                        else:
+                            print("There's syntax error of import in {}".format(infile))
+                            sys.exit(-3)
                     else:
-                        print("There's syntax error of import in {}".format(infile))
-                        sys.exit(-3)
-                else:
-                    buffer.append(line)
+                        buffer.append(line)
+
+        except FileNotFoundError:
+            print(f"There is no file found from the given path {infile}")
+            sys.exit(-5)
+
     return ''.join(buffer)
 
 
@@ -89,7 +98,7 @@ def main():
     with open(outfile, "w+") as f:
         f.write("pragma solidity ^{};\n".format(args.version))
         f.write("// SPDX-License-Identifier: agpl-3.0\n")
-        f.write(unfold_imports(imports, os.path.abspath(infile)))
+        f.write(unfold_imports(args.library, imports, os.path.abspath(infile)))
 
     print("🥡 Success! Output: {} in the {} folder".format(os.path.basename(outfile), OUTPUT_FOLDER))
 
